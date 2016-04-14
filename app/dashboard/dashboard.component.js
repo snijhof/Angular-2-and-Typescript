447c26743adf9315d41eabe2e28b9d5bacd3d8e8
Sjoerd Nijhof <sjoerdnijhof@hotmail.com>
2016-04-14 20:02:56 +0200

    Added some lorem ipsum

--- app/dashboard/dashboard.component.js
+++ app/dashboard/dashboard.component.js
@@ -30,23 +30,27 @@ System.register(['angular2/core', 'angular2/router'], function(exports_1, contex
             DashboardComponent = (function () {
                 function DashboardComponent(_router) {
                     this._router = _router;
+                    this.text = [
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id diam suscipit, consequat purus sit",
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id diam suscipit, consequat purus sit"
+                    ];
                     this.blogItems = [
-                        { "id": 1, "title": "Blog Title 1", "text": "This is the story from this awesome blog post.", "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
-                        { "id": 2, "title": "Blog Title 2", "text": "This is the story from this awesome blog post.", "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
-                        { "id": 3, "title": "Blog Title 3", "text": "This is the story from this awesome blog post.", "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
-                        { "id": 4, "title": "Blog Title 4", "text": "This is the story from this awesome blog post.", "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
-                        { "id": 5, "title": "Blog Title 5", "text": "This is the story from this awesome blog post.", "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
-                        { "id": 6, "title": "Blog Title 6", "text": "This is the story from this awesome blog post.", "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
-                        { "id": 7, "title": "Blog Title 7", "text": "This is the story from this awesome blog post.", "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
-                        { "id": 8, "title": "Blog Title 8", "text": "This is the story from this awesome blog post.", "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
-                        { "id": 9, "title": "Blog Title 9", "text": "This is the story from this awesome blog post.", "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
-                        { "id": 10, "title": "Blog Title 10", "text": "This is the story from this awesome blog post.", "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
-                        { "id": 11, "title": "Blog Title 11", "text": "This is the story from this awesome blog post.", "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
-                        { "id": 12, "title": "Blog Title 12", "text": "This is the story from this awesome blog post.", "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
-                        { "id": 13, "title": "Blog Title 13", "text": "This is the story from this awesome blog post.", "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
-                        { "id": 14, "title": "Blog Title 14", "text": "This is the story from this awesome blog post.", "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
-                        { "id": 15, "title": "Blog Title 15", "text": "This is the story from this awesome blog post.", "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
-                        { "id": 16, "title": "Blog Title 16", "text": "This is the story from this awesome blog post.", "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
+                        { "id": 1, "title": "Blog Title 1", "text": this.text, "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
+                        { "id": 2, "title": "Blog Title 2", "text": this.text, "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
+                        { "id": 3, "title": "Blog Title 3", "text": this.text, "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
+                        { "id": 4, "title": "Blog Title 4", "text": this.text, "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
+                        { "id": 5, "title": "Blog Title 5", "text": this.text, "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
+                        { "id": 6, "title": "Blog Title 6", "text": this.text, "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
+                        { "id": 7, "title": "Blog Title 7", "text": this.text, "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
+                        { "id": 8, "title": "Blog Title 8", "text": this.text, "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
+                        { "id": 9, "title": "Blog Title 9", "text": this.text, "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
+                        { "id": 10, "title": "Blog Title 10", "text": this.text, "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
+                        { "id": 11, "title": "Blog Title 11", "text": this.text, "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
+                        { "id": 12, "title": "Blog Title 12", "text": this.text, "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
+                        { "id": 13, "title": "Blog Title 13", "text": this.text, "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
+                        { "id": 14, "title": "Blog Title 14", "text": this.text, "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
+                        { "id": 15, "title": "Blog Title 15", "text": this.text, "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
+                        { "id": 16, "title": "Blog Title 16", "text": this.text, "author": "Sjoerd Nijhof", "publication": new Date(2016, 4, 7) },
                     ];
                 }
                 DashboardComponent.prototype.onSelect = function (blogItem) {
